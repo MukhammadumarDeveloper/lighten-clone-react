e095606136e79cf1f8660fd20d691d73bb3e3cdf
mrshuhrat <shuhratadm1n@gmail.com>
2020-12-22 22:02:29 +0500

    Add map section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,6 +6,9 @@ import Product4 from '../../Assets/icon/p4.png'
 import Product5 from '../../Assets/icon/p5.png'
 import Product6 from '../../Assets/icon/p6.png'
 import Product7 from '../../Assets/icon/p7.png'
+import ClientImg from '../../Assets/images/lllll.png'
+import CallIcon from '../../Assets/icon/calll.png'
+import Map from '../../Assets/images/mapimg.jpg'
 
 import Header from '../../Components/Header/Header'
 import ChooseUs from '../../Components/ChooseUs/ChooseUs'
@@ -151,6 +154,94 @@ function Home () {
             </div>
           </div>
         </div>
+      </div>
+
+      <div class="Clients_bg_white">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="title">
+                  <h3>What Clients Say?</h3>
+              </div>
+            </div>
+          </div>
+
+          <ol class="carousel-indicators">
+            <li data-target="#client_slider" data-slide-to="0" class="active"></li>
+            <li data-target="#client_slider" data-slide-to="1"></li>
+            <li data-target="#client_slider" data-slide-to="2"></li>
+          </ol>
+
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="container">
+                <div class="carousel-caption text-bg">
+                  <div class="img_bg">
+                    <i><img src={ClientImg} alt="Client img"/><span>Jone Due<br /><strong class="date">12/02/2019</strong></span></i>
+                  </div>
+                
+                  <p>You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am really satisfied with my first laptop service.<br />
+                  You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="carousel-item">
+              <div class="container">
+                <div class="carousel-caption text-bg">
+                  <div class="img_bg">
+                    <i><img src={ClientImg} alt="Client img"/><span>Jone Due<br /><strong class="date">12/02/2019</strong></span></i> 
+                  </div>
+
+                  <p>You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am really satisfied with my first laptop service.<br />
+                  You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am </p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="container">
+                <div class="carousel-caption text-bg">
+
+                  <div class="img_bg">
+                    <i><img src={ClientImg} alt="Client img"/><span>Jone Due<br /><strong class="date">12/02/2019</strong></span></i>
+                  </div>
+
+                  <p>You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am really satisfied with my first laptop service.<br />
+                  You guys rock! Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. I am </p>
+               
+                </div>
+              </div>
+            </div>
+        </div>
+
+      </div>
+      </div> 
+
+
+
+      <div class="container">
+        <div class="yellow_bg">
+          <div class="row">
+            <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+              <div class="yellow-box">
+                <h3>REQUEST A FREE QUOTE<i><img src={CallIcon} alt="call icon" /></i></h3> 
+                <p>Get answers and advice from people you want it from.</p>
+              </div>
+            </div>
+            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+              <div class="yellow-box">
+                <a href="/">Get  Quote</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container-fluid padi">
+         <div class="map">
+            <img src={Map} alt="img"/>
+         </div>
       </div>
     </>
   )
