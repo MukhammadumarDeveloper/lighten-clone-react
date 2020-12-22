1b97a50ef46724088b0d4200d860fd469992bdca
mrshuhrat <shuhratadm1n@gmail.com>
2020-12-22 21:14:49 +0500

    Add Product section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,12 @@
 import Banner2 from '../../Assets/images/banner2.jpg'
+import Product1 from '../../Assets/icon/p1.png'
+import Product2 from '../../Assets/icon/p2.png'
+import Product3 from '../../Assets/icon/p3.png'
+import Product4 from '../../Assets/icon/p4.png'
+import Product5 from '../../Assets/icon/p5.png'
+import Product6 from '../../Assets/icon/p6.png'
+import Product7 from '../../Assets/icon/p7.png'
+
 import Header from '../../Components/Header/Header'
 import ChooseUs from '../../Components/ChooseUs/ChooseUs'
 import ServiceProcess from '../../Components/ServiceProcess/ServiceProcess'
@@ -7,6 +15,7 @@ function Home () {
 
   return (
     <>
+
       <Header />
       
       <section>
@@ -65,6 +74,84 @@ function Home () {
       <ChooseUs  />
 
       <ServiceProcess />
+
+      <div class="product">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="title">
+                     <h2>our <strong class="black">products</strong></h2>
+                     <span>We package the products with best services to make you a happy customer.</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="product-bg">
+        <div class="product-bg-white">
+          <div class="container">
+            <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product1} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product2} alt="Product icon"/></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product3} alt="Product icon"/></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product4} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product5} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product2} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product6} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+               <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                  <div class="product-box">
+                     <i><img src={Product7} alt="Product icon" /></i>
+                     <h3>Norton Internet Security</h3>
+                     <span>$25.00</span>
+                  </div>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 
