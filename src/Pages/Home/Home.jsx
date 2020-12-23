dee3e9241f8cb9dfc9977d663d73604c5d21ad84
mrshuhrat <shuhratadm1n@gmail.com>
2020-12-23 15:43:27 +0500

    Sileder is Working

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -23,55 +23,58 @@ function Home () {
       <Header />
       
       <section>
-        <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
+        <div className="conatiner">
+          <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
+            <div class="carousel-inner">
 
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img class="first-slide" src={Banner2} alt="First slide" />
-              <div class="container">
-                <div class="carousel-caption relative">
-                  <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
-                    <strong class="yellow_bold">Product </strong></h1>
-                  <p>It is a long established fact that a r <br />
-                    eader will be distracted by the readable content of a page </p>
-                  <a href="/">see more Products</a>
+              <div class="carousel-item active">
+                <img class="first-slide" src={Banner2} alt="First slide" />
+                <div class="container">
+                  <div class="carousel-caption relative">
+                    <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
+                      <strong class="yellow_bold">Product </strong></h1>
+                    <p>It is a long established fact that a r <br />
+                      eader will be distracted by the readable content of a page </p>
+                    <a href="/">see more Products</a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="carousel-item">
-              <img class="second-slide" src="images/banner2.jpg" alt="Second slide" />
-              <div class="container">
-                <div class="carousel-caption relative">
-                  <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
-                           <strong class="yellow_bold">Product </strong></h1>
-                  <p>It is a long established fact that a r <br />
-                          eader will be distracted by the readable content of a page </p>
-                  <a href="/">see more Products</a>
+
+              <div class="carousel-item">
+                <img class="second-slide" src={Banner2} alt="Second slide" />
+                <div class="container">
+                  <div class="carousel-caption relative">
+                    <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
+                      <strong class="yellow_bold">Product </strong></h1>
+                    <p>It is a long established fact that a r <br />
+                      eader will be distracted by the readable content of a page </p>
+                    <a href="/">see more Products</a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="carousel-item">
-              <img class="third-slide" src="images/banner2.jpg" alt="Third slide" />
-              <div class="container">
-                <div class="carousel-caption relative">
-                  <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
-                    <strong class="yellow_bold">Product </strong></h1>
-                  <p>It is a long established fact that a r <br />
-                    eader will be distracted by the readable content of a page </p>
-                  <a href="/">see more Products</a>
+
+              <div class="carousel-item">
+                <img class="third-slide" src={Banner2} alt="Third slide" />
+                <div class="container">
+                  <div class="carousel-caption relative">
+                    <h1>Our <br /> <strong class="black_bold">Latest </strong><br />
+                      <strong class="yellow_bold">Product </strong></h1>
+                    <p>It is a long established fact that a r <br />
+                      eader will be distracted by the readable content of a page </p>
+                    <a href="/">see more Products</a>
+                  </div>
                 </div>
               </div>
+
             </div>
+            <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+              <i class='fa fa-angle-right'></i>
+            </a>
+            <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+              <i class='fa fa-angle-left'></i>
+            </a>
 
           </div>
-
-          <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-            <i class='fa fa-angle-right'></i>
-          </a>
-          <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-            <i class='fa fa-angle-left'></i>
-          </a>
-
         </div>
       </section>
 
@@ -167,11 +170,11 @@ function Home () {
             </div>
           </div>
 
-          <ol class="carousel-indicators">
+          {/* <ol class="carousel-indicators">
             <li data-target="#client_slider" data-slide-to="0" class="active"></li>
             <li data-target="#client_slider" data-slide-to="1"></li>
             <li data-target="#client_slider" data-slide-to="2"></li>
-          </ol>
+          </ol> */}
 
           <div class="carousel-inner">
             <div class="carousel-item active">
